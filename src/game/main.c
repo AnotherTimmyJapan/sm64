bb5fd28560ebac97ac53fdc1bc962395a5c0a075
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-02-23 19:58:59 +0900

    Debug

--- src/game/main.c
+++ src/game/main.c
@@ -56,7 +56,7 @@ s8 gDebugLevelSelect = FALSE;
 s8 D_8032C650 = 0;
 
 s8 gShowProfiler = FALSE;
-s8 gShowDebugText = FALSE;
+s8 gShowDebugText = TRUE;
 
 // unused
 void handle_debug_key_sequences(void) {
